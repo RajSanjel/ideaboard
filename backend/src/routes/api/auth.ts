75c4141a00bbfb6b46ac86626524c77092bcb8a5
Raj Sanjel <rajsanjel201@gmail.com>
2026-07-07 17:03:54 +0545

    fix: fixed the wrong http method being used

--- backend/src/routes/api/auth.ts
+++ backend/src/routes/api/auth.ts
@@ -7,6 +7,6 @@ const router: Router = Router();
 router.post("/register", validateSignUp, AuthController.register);
 router.post("/login", validateLogin, AuthController.login);
 router.post("/logout", AuthController.logout);
-router.post("/me", AuthController.me);
+router.get("/me", AuthController.me);
 
 export default router;
